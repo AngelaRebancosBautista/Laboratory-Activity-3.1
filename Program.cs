5ad008aee596f380bf51532798f41edc572cfa6f
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-08-09 08:41:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORY_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> studentNames = new List<string>();
+            int aNamesCount = 0;
+
+            Console.WriteLine("Enter student names (type 'done' to finish):");
+
+            while (true)
+
+            {
+                string input = Console.ReadLine();
+
+                if (input?.ToLower() == "done")
+                {
+                    break;
+                }
+
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    string uppercaseName = input.ToUpper();
+                    studentNames.Add(uppercaseName);
+
+                    if (uppercaseName.StartsWith("A"))
+                    {
+                        aNamesCount++;
+                    }
+                }
+            }
+            Console.WriteLine("\nAll student names in uppercase:");
+            foreach (string name in studentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine($"\nNumber of names starting with 'A': {aNamesCount}");
+        }
+    }
+}
+            
